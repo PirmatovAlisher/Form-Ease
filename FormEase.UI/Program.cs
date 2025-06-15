77fbd4e6000edadfed048ef620bb761e14f4ed17
PirmatovAlisher <pirmatovalisher000@gmail.com>
2025-06-15 16:33:39 +0500

    Initial Radzen layout setup

--- FormEase.UI/Program.cs
+++ FormEase.UI/Program.cs
@@ -4,110 +4,113 @@ using FormEase.UI.Components;
 using FormEase.UI.Components.Account;
 using FormEase.UI.Extensions;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Radzen;
 
 namespace FormEase.UI
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRadzenComponents();
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+			// Add services to the container.
+			builder.Services.AddRadzenComponents();
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
 
-            builder.Services.AddCascadingAuthenticationState();
-            builder.Services.AddScoped<IdentityUserAccessor>();
-            builder.Services.AddScoped<IdentityRedirectManager>();
-            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+			builder.Services.AddCascadingAuthenticationState();
+			builder.Services.AddScoped<IdentityUserAccessor>();
+			builder.Services.AddScoped<IdentityRedirectManager>();
+			builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
-            builder.Services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
-                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-                })
-                .AddIdentityCookies();
+			builder.Services.AddAuthentication(options =>
+				{
+					options.DefaultScheme = IdentityConstants.ApplicationScheme;
+					options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+				})
+				.AddIdentityCookies();
 
             builder.Services.AddAuthorization();
 
             builder.Services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = "/Account/Login";
-                options.AccessDeniedPath = "/Account/AccessDenied";
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // Force HTTPS cookies
-                options.Cookie.SameSite = SameSiteMode.Lax; // Allow top-level navigation
-            });
+			{
+				options.LoginPath = "/Account/Login";
+				options.AccessDeniedPath = "/Account/AccessDenied";
+				options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+				options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // Force HTTPS cookies
+				options.Cookie.SameSite = SameSiteMode.Lax; // Allow top-level navigation
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("DefaultConnection not set");
-            if (!builder.Environment.IsDevelopment())
-            {
-                connectionString = ConnectionStringHandler.GetRenderConnectionString();
-            }
+			});
 
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(connectionString, x => x.MigrationsAssembly("FormEase.Infrastructure.PostgreSQL")));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("DefaultConnection not set");
+			if (!builder.Environment.IsDevelopment())
+			{
+				connectionString = ConnectionStringHandler.GetRenderConnectionString();
+			}
 
-            builder.Services.AddIdentityCore<ApplicationUser>(options =>
-            options.SignIn.RequireConfirmedAccount = false)
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddSignInManager()
-                .AddDefaultTokenProviders();
+			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseNpgsql(connectionString, x => x.MigrationsAssembly("FormEase.Infrastructure.PostgreSQL")));
+			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+			builder.Services.AddIdentityCore<ApplicationUser>(options =>
+			options.SignIn.RequireConfirmedAccount = false)
+				.AddRoles<IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddSignInManager()
+				.AddDefaultTokenProviders();
 
-            var app = builder.Build();
+			builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
-            // Add this at the TOP of middleware pipeline
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
+			var app = builder.Build();
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                db.Database.Migrate();
-            }
+			// Add this at the TOP of middleware pipeline
+			app.UseForwardedHeaders(new ForwardedHeadersOptions
+			{
+				ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+			});
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			using (var scope = app.Services.CreateScope())
+			{
+				var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+				db.Database.Migrate();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseMigrationsEndPoint();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseRouting();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+			app.UseRouting();
 
-            app.UseAntiforgery();
+			app.UseAuthentication();
+			app.UseAuthorization();
 
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
+			app.UseAntiforgery();
 
-            // Add additional endpoints required by the Identity /Account Razor components.
-            app.MapAdditionalIdentityEndpoints();
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
 
-            app.Run();
-        }
-    }
+			// Add additional endpoints required by the Identity /Account Razor components.
+			app.MapAdditionalIdentityEndpoints();
+
+			app.Run();
+		}
+	}
 }
